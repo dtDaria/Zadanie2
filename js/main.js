b9e77dc8d6ceb40f8d1233e08eb19c01cfe76145
gr413_toddm <38tosha@gmail.com>
2023-02-08 13:58:12 +0700

    2 неделя топ

--- js/main.js
+++ js/main.js
@@ -2,32 +2,52 @@ let eventBus = new Vue()
 Vue.component('product-review', {
     template: `
 <div class="modal-body">    
-        <form class="review-form" @submit.prevent="onSubmit">
+        <form class="review-form" @submit.prevent="onSubmit"> 
+           <p v-if="errors.length">
+    <b>Введите всё правильно:</b>
+    <ul>
+        <li v-for="error in errors">{{ error }}</li>
+    </ul>
+</p>
 </p>
  <p>
+  <p>
+   <label for="name">Название:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
    <label for="review">Заметка:</label>
    <textarea id="review" v-model="review"></textarea>
  </p>
+
  <p>
-   <input type="submit" value="Submit"> 
+<input type="submit" value="Сохранить">
  </p>
 </form>
       </div>
  `,
     data() {
         return {
+            counter: 0,
+            name:null,
             review: null,
             errors: [],
         }
     },
     methods: {
         onSubmit() {
-            console.log(1)
-            let productReview = {
-                review: this.review,
+            this.errors = [] // очищение при исправлении ошибки
+            if (this.name && this.review) {
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                }
+                eventBus.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+            }else {
+                if (!this.name) this.errors.push("Имя отсутствует")
+                if (!this.review) this.errors.push("Заметка отсутствует")
             }
-            eventBus.$emit('review-submitted', productReview)
-            this.review = null
         }
     }
 })
@@ -37,15 +57,20 @@ Vue.component('product', {
         <div class="product-info">
             <div>
             <h2>Чем хочешь поделиться?</h2>
+            
             <ul>
-                  <li v-for="review in reviews">
-                  <p>Заметка: {{ review.rating }}</p>
-                  <p>{{ review.review }}</p>
+                <li v-for="review in reviews">
+                  <p>Название: {{ review.name }}</p>
+                  <p>Заметка: {{ review.review }}</p>
                   </li>
             </ul>
             </div>   
+            
         </div>
+        
    </div>
+   
+   
  `,
     data() {
         return {
@@ -58,15 +83,22 @@ Vue.component('product', {
             this.reviews.push(productReview)
         })
     }
+
 })
 let app = new Vue({
     el: '#app',
     data: {
         cart: [],
     },
-})
 
-// https://learn.javascript.ru/event-delegation
+})
+let num = 0, submit = document.querySelector('[type=submit]');
+submit.onclick = function () {
+    num++, num > 3 ? this.disabled = true : '';
+};
+
+
+
 
 
 
